94103ef427d8a19308ce714e1caca9470eb65681
yinzida <1975793885@qq.com>
2024-04-04 21:22:25 +0800

    finish util's info

--- kernel/fs.h
+++ kernel/fs.h
@@ -12,11 +12,11 @@
 // mkfs computes the super block and builds an initial file system. The
 // super block describes the disk layout:
 struct superblock {
-  uint magic;        // Must be FSMAGIC
-  uint size;         // Size of file system image (blocks)
-  uint nblocks;      // Number of data blocks
-  uint ninodes;      // Number of inodes.
-  uint nlog;         // Number of log blocks
+  uint magic;        // Must be FSMAGIC                     魔数
+  uint size;         // Size of file system image (blocks)  文件系统所有块
+  uint nblocks;      // Number of data blocks               数据块的数量
+  uint ninodes;      // Number of inodes.                   inode的数目
+  uint nlog;         // Number of log blocks                log的数目
   uint logstart;     // Block number of first log block
   uint inodestart;   // Block number of first inode block
   uint bmapstart;    // Block number of first free map block
@@ -30,31 +30,38 @@ struct superblock {
 
 // On-disk inode structure
 struct dinode {
-  short type;           // File type
+  short type;           // File type  表示文件的类型。常见的类型包括普通文件、目录、字符设备、块设备等。
   short major;          // Major device number (T_DEVICE only)
   short minor;          // Minor device number (T_DEVICE only)
-  short nlink;          // Number of links to inode in file system
+  short nlink;          // Number of links to inode in file system 表示文件系统中连接到该索引节点的硬链接数目。
   uint size;            // Size of file (bytes)
-  uint addrs[NDIRECT+1];   // Data block addresses
+  uint addrs[NDIRECT+1];   // Data block addresses 包含数据块地址的数组。在这个数组中，前 NDIRECT 个元素存储直接数据块的地址，最后一个元素存储间接数据块的地址。数据块地址指向存储实际文件数据的磁盘块。
 };
 
-// Inodes per block.
+/*
+主设备号用于标识设备的整体类型或分类，它指示设备驱动程序应该使用哪个特定的驱动程序来管理该设备。
+主设备号通常与设备的类型相关，例如硬盘、光驱、网络接口等。
+次设备号用于标识特定类型的设备中的不同实例或不同分区。它用于细分相同类型的设备，
+以便操作系统能够区分它们并正确地与其进行通信。例如，对于块设备（如硬盘），次设备号可以用来标识不同的分区。
+*/
+
+// Inodes per block. 计算每个数据块中的索引节点数量。
 #define IPB           (BSIZE / sizeof(struct dinode))
 
-// Block containing inode i
+// Block containing inode i 计算包含索引节点 i 的块号
 #define IBLOCK(i, sb)     ((i) / IPB + sb.inodestart)
 
-// Bitmap bits per block
+// Bitmap bits per block 计算每个位图块中的位数数量
 #define BPB           (BSIZE*8)
 
-// Block of free map containing bit for block b
+// Block of free map containing bit for block b  计算包含块 b 的空闲位图块号
 #define BBLOCK(b, sb) ((b)/BPB + sb.bmapstart)
 
 // Directory is a file containing a sequence of dirent structures.
 #define DIRSIZ 14
 
 struct dirent {
-  ushort inum;
-  char name[DIRSIZ];
+    ushort inum;        // 目录条目对应的索引节点（inode）号
+    char name[DIRSIZ];  // 存储目录条目的名称
 };
 
--- kernel/stat.h
+++ kernel/stat.h
@@ -2,6 +2,7 @@
 #define T_FILE    2   // File
 #define T_DEVICE  3   // Device
 
+// 表示文件的状态信息
 struct stat {
   int dev;     // File system's disk device
   uint ino;    // Inode number
